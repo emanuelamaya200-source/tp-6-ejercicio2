ff7413b2332a7dc792526d54afba0d16c1101183
Lucas <serranolucas1000@gmail.com>
2025-09-13 20:35:34 -0300

    Jframe añadido

--- JavaApplication82/src/proyecto/Principal.java
+++ JavaApplication82/src/proyecto/Principal.java
@@ -4,6 +4,8 @@
  */
 package proyecto;
 
+import Visual.VentanaPrincipal;
+
 /**
  *
  * @author Ema
@@ -14,7 +16,8 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        VentanaPrincipal pantalla = new VentanaPrincipal();
+        pantalla.setVisible(true);
     }
     
 }
